e37f96e1f8cb89921da1d194cb3734cd25344272
minho <minho.yeon@gmail.com>
2024-05-25 16:25:58 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,15 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+app.use(express.json()) //For JSON requests
+app.use(express.urlencoded({extended: true}));
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://minhoyeon:1111@cluster0.s1yeoet.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-  // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected..'))
+mongoose.connect(config.mongoURI)
+.then(() => console.log('MongoDB Connected..'))
 .catch(err => console.log(err))
 
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!  hellosdafsaasdfsdfdf')
 })
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+
+  user.save()
+  .then(() => {
+    res.status(200).json({
+      success: true,
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+    res.json({
+      success: false,
+      err: err,
+    });
+  });
+});
+  
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- models/User.js
+++ models/User.js
@@ -32,4 +32,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model('User', userSchema)
 
-module.exporth = {User}
+module.exports = {User}
